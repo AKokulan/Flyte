4d2fb092e560a1ceebd1418575092f03ffadb0f6
Kokulan Ampikanithy <kokul1991@gmail.com>
2024-07-26 00:15:01 +0530

    k8 master setup file

---
+++ k8v1.27_master_setup.sh
@@ -0,0 +1,92 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+
+# Step 2: Disable Swap (all nodes)
+# To enhance Kubernetes performance, disable swap and set essential kernel parameters. Run the following commands on all nodes to disable all swaps:
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+
+# Step 3: Add Kernel Parameters (all nodes)
+# Load the required kernel modules on all nodes:
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+
+# Configure the critical kernel parameters for Kubernetes using the following:
+
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOF
+
+# Then, reload the changes:
+sudo sysctl --system
+
+
+#Step 4: Install Containerd Runtime (all nodes)
+
+#We are using the containerd runtime. Install containerd and its dependencies with the following commands:
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+
+
+#Enable the Docker repository:
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+#Update the package list and install containerd:
+sudo apt update
+sudo apt install -y containerd.io
+
+# Configure containerd to start using systemd as cgroup:
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+# Restart and enable the containerd service:
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+
+sudo apt-get update
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Install kubernetes v1.27
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+
+# This overwrites any existing configuration in /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+
+sudo apt remove kubelet kubeadm kubectl
+
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+sudo systemctl enable --now kubelet
+
+
+#start master node
+sudo kubeadm init --kubernetes-version=v1.27.0
+
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+# Step :9 Install Kubernetes Network Plugin (master node)
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
+#print join command
+kubeadm token create --print-join-command
+
